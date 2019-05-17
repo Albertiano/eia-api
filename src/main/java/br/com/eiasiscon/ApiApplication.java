5c54013a5ec1ad29022f1ec9d2f61dc30d00fda0
Albertiano <albertiano.net@gmail.com>
2019-05-17 15:11:27 -0300

    dfsfdssfdafdsafdsafds

--- src/main/java/br/com/eiasiscon/ApiApplication.java
+++ src/main/java/br/com/eiasiscon/ApiApplication.java
@@ -1,10 +1,17 @@
 package br.com.eiasiscon;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import org.bson.types.Decimal128;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.mongodb.core.convert.CustomConversions;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -18,4 +25,32 @@ public class ApiApplication extends SpringBootServletInitializer {
 	public static void main(String[] arg) {
 		SpringApplication.run(ApiApplication.class, arg);
 	}
+	
+	@ReadingConverter
+	public class Decimal128ReadConverter implements Converter<Decimal128, BigDecimal> {
+		  
+		  public BigDecimal convert(Decimal128 s) {
+              return s==null ? null : s.bigDecimalValue();
+          }
+	}
+	
+	@Bean
+    CustomConversions customConverions() {
+		
+        Converter<Decimal128, BigDecimal> decimal128ToBigDecimal = new Converter<Decimal128, BigDecimal>() {
+            @Override
+            public BigDecimal convert(Decimal128 s) {
+                return s==null ? null : s.bigDecimalValue();
+            }
+        };
+        
+        Converter<BigDecimal, Decimal128> bigDecimalToDecimal128 = new Converter<BigDecimal, Decimal128>() {
+            @Override
+            public Decimal128 convert(BigDecimal s) {
+                return s==null ? null : new Decimal128(s);
+            }
+        };
+        
+        return new CustomConversions(Arrays.asList(decimal128ToBigDecimal, bigDecimalToDecimal128));
+    }
 }
